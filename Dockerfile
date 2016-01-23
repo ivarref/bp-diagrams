04e344e8aac7757d4696d622e9904402ce953239
Ivar Refsdal <ivar.refsdal@conax.com>
2016-01-23 18:31:06 +0100

    Have OK working PNG export

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+# Running this manually:
+# create docker machine locally:
+# docker-machine create --driver virtualbox --virtualbox-memory "8096" default
+# eval $(docker-machine env default)
+
+# run phantomjs as a daemon:
+# docker run -d --name=phantomjs cmfatih/phantomjs /usr/bin/phantomjs --webdriver=8910
+
+# build server:
+# docker build --tag=ivarref/bp-diagrams .
+
+# run the server:
+# docker run --publish=8080:8080 --link=phantomjs ivarref/bp-diagrams
+
+# then you may access http://$(docker-machine ip default):8080/
+
+# build and run:
+# docker build --tag=ivarref/bp-diagrams . && docker run --publish=8080:8080 --link=phantomjs ivarref/bp-diagrams
+
+
+FROM picoded/ubuntu-openjdk-8-jdk
+
+RUN apt-get update -qq
+RUN apt-get install -y git curl telnet
+
+ENV MAVEN_VERSION 3.3.9
+
+RUN curl -fsSL https://archive.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz | tar xzf - -C /usr/share \
+  && mv /usr/share/apache-maven-$MAVEN_VERSION /usr/share/maven \
+  && ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
+
+ENV MAVEN_HOME /usr/share/maven
+
+RUN git clone https://github.com/ivarref/bp-diagrams.git
+RUN cd bp-diagrams && mvn clean package
+
+EXPOSE 8080
+COPY . bp-diagrams/
+RUN cd bp-diagrams && mvn clean package
+
+CMD /bp-diagrams/target/appassembler/bin/app -DselfIp="$(hostname -i)"
--- src/main/java/no/ivarref/PngServer.java
+++ src/main/java/no/ivarref/PngServer.java
@@ -1,5 +1,9 @@
 package no.ivarref;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -7,65 +11,100 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
 import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class PngServer {
 
+    final static Object lock = new Object();
+
     public static void main(String[] args) throws Exception {
-        System.out.println("hello world ;-)");
-        Object lock = new Object();
+        String selfIp = Arrays.asList(args)
+                .stream()
+                .filter(s -> s.startsWith("-DselfIp="))
+                .map(s -> s.split("=")[1])
+                .findFirst().orElse("localhost");
+
         Server server = new Server(8080);
-        FirefoxDriver driver = new FirefoxDriver();
+        boolean isDocker = new File("/bp-diagrams").exists();
+        System.out.println("starting webserver with selfIp = " + selfIp + ", isDocker = " + isDocker);
 
-        server.setHandler(new HandlerList() {
-            {
-                addHandler(new AbstractHandler() {
-                    @Override
-                    public void handle(String p, Request request, HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
-                        if ("true".equalsIgnoreCase(request.getParameter("image"))) {
-                            response.setStatus(200);
-                            request.setHandled(true);
-                            String url = String.join("&", new TreeSet<>(request.getParameterMap().keySet())
-                                    .stream()
-                                    .filter(s ->
-                                            s.matches("^[a-zA-Z_]+$")
-                                                    && !("image".equalsIgnoreCase(s))
-                                                    && request.getParameter(s).matches("^[a-zA-Z0-9_]+$"))
-                                    .map(s -> s + "=" + request.getParameter(s))
-                                    .collect(Collectors.toList()));
-                            System.out.println("generating image for: " + url);
-
-                            byte[] screenShot;
-                            synchronized (lock) {
-                                try {
-                                    driver.get("http://localhost:8000/?" + url);
-                                    screenShot = driver.getScreenshotAs(OutputType.BYTES);
-                                } finally {
-                                    //driver.quit();
-                                }
-                            }
-                            System.out.println("length of screenshot: " + screenShot.length);
-
-                            response.setContentType("image/png");
-                            response.setContentLength(screenShot.length);
-                            response.getOutputStream().write(screenShot);
-                        }
-                    }
-                });
-                addHandler(new ResourceHandler() {
-                    {
-                        setResourceBase(".");
-                    }
-                });
+        if (isDocker) {
+            for (String s : Arrays.asList("data/data.tsv", "d3.v3.min.js", "index.html")) {
+                FileUtils.copyFile(new File("/bp-diagrams/" + s), new File("/bp-diagrams/target/web/" + s));
             }
-        });
+        }
+
+        RemoteWebDriver driver = isDocker ? new RemoteWebDriver(new URL("http://phantomjs:8910"), DesiredCapabilities.phantomjs()) : new FirefoxDriver();
+
+        server.setHandler(new HandlerList() {{
+                addHandler(generatePNGHandler(driver, selfIp));
+                addHandler(new ResourceHandler() {{ setResourceBase(isDocker ? "/bp-diagrams/target/web" : "."); }});
+        }});
+
         server.start();
         server.join();
+    }
+
+    private static AbstractHandler generatePNGHandler(final RemoteWebDriver driver, final String selfIp) {
+        LoadingCache<String, ByteContainer> pngCache = CacheBuilder.newBuilder()
+                .maximumSize(10000)
+                .build(new CacheLoader<String, ByteContainer>() {
+                            public ByteContainer load(String key) throws Exception {
+                                synchronized (lock) {
+                                    long start = System.currentTimeMillis();
+                                    driver.get("http://" + selfIp + ":8080/?" + key);
+                                    byte[] screenShot = driver.getScreenshotAs(OutputType.BYTES);
+                                    long diff = System.currentTimeMillis() - start;
+                                    System.out.println("time to take screenshot: " + diff + " ms, length of screenshot: " + screenShot.length + ", url: " + key);
+                                    return new ByteContainer(screenShot);
+                                }
+                            }
+                        });
+
+        return new AbstractHandler() {
+            @Override
+            public void handle(String p, Request request, HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
+                if ("true".equalsIgnoreCase(request.getParameter("image"))) {
+                    request.setHandled(true);
+                    String url = String.join("&", new TreeSet<>(request.getParameterMap().keySet())
+                            .stream()
+                            .filter(s -> s.matches("^[a-zA-Z_]+$") && !("image".equalsIgnoreCase(s)) && request.getParameter(s).matches("^[a-zA-Z0-9_,]+$"))
+                            .map(s -> s + "=" + request.getParameter(s))
+                            .collect(Collectors.toList()));
+
+                    try {
+                        byte[] screenShot = pngCache.get(url).png;
+                        response.setContentType("image/png");
+                        response.setContentLength(screenShot.length);
+                        response.getOutputStream().write(screenShot);
+                        response.setStatus(200);
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                        System.err.println("error generating PNG for: " + url);
+                        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                    }
+                }
+            }
+        };
+    }
+
+    public static class ByteContainer {
+        public final byte[] png;
+
+        public ByteContainer(byte[] png) {
+            this.png = png;
+        }
     }
 }
