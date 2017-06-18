29d00da59dff0ff4ad792c8c4952c5c801592f3d
Ivar Refsdal <ivar.refsdal@nsd.no>
2017-06-18 18:26:06 +0200

    ...

--- gen_data2.py
+++ gen_data2.py
@@ -50,11 +50,12 @@ def get_c3_to_c2_map():
 c3_to_c2 = get_c3_to_c2_map()
 
 coal = csv2map_from_bp_csv('data/BP_coal_consumption_mtoe.csv', 'coal')
-gas = csv2map_from_bp_csv('data/BP_gas_consumption_mtoe.csv', 'gas')
-hydro = csv2map_from_bp_csv('data/BP_hydro_consumption_mtoe.csv', 'hydro')
-nuclear = csv2map_from_bp_csv('data/BP_nuclear_consumption_mtoe.csv', 'nuclear')
 oil = csv2map_from_bp_csv('data/BP_oil_consumption_mtoe.csv', 'oil')
+gas = csv2map_from_bp_csv('data/BP_gas_consumption_mtoe.csv', 'gas')
+nuclear = csv2map_from_bp_csv('data/BP_nuclear_consumption_mtoe.csv', 'nuclear')
+hydro = csv2map_from_bp_csv('data/BP_hydro_consumption_mtoe.csv', 'hydro')
 renewables = csv2map_from_bp_csv('data/BP_other_renewables_consumption_mtoe.csv', 'renewables')
+
 co2 = csv2map_from_bp_csv('data/BP_co2_emissions.csv', 'co2')
 
 coal_prod = csv2map_from_bp_csv('data/BP_coal_production_mtoe.csv', 'coal_prod')
@@ -68,7 +69,7 @@ def get_country_codes():
     for item in all_items:
         for row in item:
             for k in row.keys():
-                if k in country_codes or k == 'YEAR' or k == 'TYPE':
+                if k in country_codes or k == 'YEAR' or k == 'TYPE' or k.startswith('BP_'):
                     continue
                 else:
                     country_codes.append(k)
@@ -105,14 +106,29 @@ def get_flat_values():
 
 flat_values = get_flat_values()
 
-def make_group(group):
-    print("Making group %s ..." % (group))
-    for year in range(1965, 1+max([int(x['year']) for x in flat_values])):
-        properties = dict([(x['type'], x['value']) for x in flat_values if x['year'] == str(year) and x['group'] == group])
-        print("Making group %s ... year %s => %s" % (group, year, str(properties)))
+def data_group_year(group, year):
+    properties = dict([(x['type'], x['value']) for x in flat_values if x['year'] == str(year) and x['group'] == group])
+    properties['group'] = group
+    properties['year'] = str(year)
+    return properties
 
-make_group(get_country_codes()[0])
+# def make_group(group):
+#     print("Making group %s ..." % (group))
+#     vals = []
+#     for year in range(1965, 1+max([int(x['year']) for x in flat_values])):
+#         properties = dict([(x['type'], x['value']) for x in flat_values if x['year'] == str(year) and x['group'] == group])
+#         properties['group'] = group
+#         properties['year'] = str(year)
+#         expected_properties = ['coal', 'oil', 'gas', 'hydro', 'nuclear', 'renewables', 'coal_prod', 'gas_prod', 'oil_prod', 'gdp', 'population', 'co2']
+#         missing_properties = [x for x in expected_properties if not x in properties.keys()]
+#         if len(missing_properties) > 0:
+#             print("Group %s year %s => missing %d properties => %s" % (group, str(year), len(missing_properties), str(missing_properties)))
+#         vals.append(properties)
+#     return vals
+    #print("Making group %s ... year %s => %s" % (group, year, str(len(properties.keys()))))
+
+print(data_group_year('IQ', '2016'))
 
 #import ipdb; ipdb.set_trace()
-#print(get_country_codes())
+print(get_country_codes())
 
